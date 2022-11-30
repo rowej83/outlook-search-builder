142a84246e411ea5acd1673316eeff3a39fc1817
Jason Rowe <rowej83@gmail.com>
2022-11-30 15:54:19 -0500

    Update OptionsArea.tsx

--- src/components/OptionsArea.tsx
+++ src/components/OptionsArea.tsx
@@ -1,50 +1,63 @@
 import React from "react";
 import DatePickerArea from "./Options/DatePickerArea";
 import AttachmentsArea from "./Options/AttachmentsArea";
-import {selectedOptionsAreaState} from "../store/atoms";
-import {useRecoilState} from "recoil";
+import { useRecoilState } from "recoil";
+import { optionsState } from "./../store/atoms";
+import { cloneDeep } from "lodash";
 
-
+// import { MemoryRouter as Router, Switch, Route, Link } from "react-router-dom";
 type Props = {};
 
 export default function OptionsArea(props: Props) {
-    const [selectedOptionsArea, setSelectedOptionsArea] = useRecoilState(selectedOptionsAreaState);
-    const styles = {
-
-        attachmentLinkBackGround: {
-            "background-color": selectedOptionsArea === "attachments" ? "#252525" : "#636363"
-        } as React.CSSProperties,
-        dataRangeLinkBackGround: {
-            "background-color": selectedOptionsArea === "dateRange" ? "#252525" : "#636363"
-        } as React.CSSProperties,
-
-
-    }
-    return (
-        <>
-            <h2 className={"optionsHeader"}>Options </h2>
-
-
-            <div className={"routerLinksContainer"}>
-                <ul className={"routerLinks"}>
-                    <li style={styles.attachmentLinkBackGround}>
-                        <span onClick={() => {
-                            setSelectedOptionsArea('attachments')
-                        }}>Attachments</span>
-
-                    </li>
-                    <li style={styles.dataRangeLinkBackGround}>
-                        <span onClick={() => {
-                            setSelectedOptionsArea('dateRange')
-                        }}>Date Range</span>
-
-                    </li>
-                </ul>
-            </div>
-
-            {selectedOptionsArea === "attachments" && <AttachmentsArea/>}
-            {selectedOptionsArea === "dateRange" && <DatePickerArea/>}
-
-        </>
-    );
+  const [optionsObject, setOptionsObject] = useRecoilState(optionsState);
+  function onChangeAttachmentValue(
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void {
+    let tempOptionsObject = cloneDeep(optionsObject);
+    tempOptionsObject.hasAttachment = !optionsObject.hasAttachment;
+    console.log(tempOptionsObject);
+    setOptionsObject(tempOptionsObject);
+  }
+  return (
+    <>
+      <h2 className={"optionsHeader"}>Options </h2>
+      <div className={"attachmentsArea"}>
+        {/* <p>Include emails that:</p> */}
+        <div className={"attachmentOptions"}>
+          <div>
+            <input
+              type="checkbox"
+              onChange={onChangeAttachmentValue}
+              name="attachment"
+              id="withOrWithOutAttachment"
+              checked={optionsObject.hasAttachment}
+            />
+            <label htmlFor="withOrWithOutAttachment">
+              Has Attachments only
+            </label>
+          </div>
+        </div>
+      </div>
+      {/* <Router>
+        <div className={"routerLinksContainer"}>
+          <ul className={"routerLinks"}>
+            <li>
+              <Link to="/attachments">Attachments</Link>
+            </li>
+            <li>
+              <Link to="/dateoptions">Date Range</Link>
+            </li>
+          </ul>
+        </div>
+        <Switch>
+          <Route exact path="/attachments">
+            <AttachmentsArea />
+          </Route>
+          <Route path="/dateoptions">
+            <DatePickerArea />
+          </Route>
+        </Switch>
+      </Router> */}
+    </>
+  );
 }
