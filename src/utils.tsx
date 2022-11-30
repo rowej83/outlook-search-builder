1ffde98325b473bd51050e46ab9a86b13e5d9f78
Jason Rowe <rowej83@gmail.com>
2022-11-30 15:53:45 -0500

    Update utils.tsx

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,44 +1,9 @@
-interface optionsObjectType {
-    hasAttachments: string;
-    hasRange: boolean;
-    rangeStart: Date | null;
-    rangeEnd: Date | null;
+type optionsObjectType = {
+  hasAttachment: boolean;
+  hasRange: boolean;
+  rangeStart: Date;
+  rangeEnd: Date;
 };
-
-export function attachmentsCheck(inputString: string, optionsObject: optionsObjectType): string {
-
-    switch (optionsObject.hasAttachments) {
-
-        case "withOrWithOutAttachment":
-            return inputString;
-            break;
-        case "withAttachment":
-            return inputString + ` AND hasattachments:yes`;
-            break;
-        case "withOutAttachment":
-            return inputString + ` AND hasattachments:no`;
-            break;
-    }
-    return inputString;
-}
-
-export function getFormattedDate(date: Date): string {
-    let year = date.getFullYear();
-    let month = (1 + date.getMonth()).toString().padStart(2, '0');
-    let day = date.getDate().toString().padStart(2, '0');
-
-    return month + '/' + day + '/' + year;
-}
-
-export function dateRangeCheck(inputString: string, optionsObject: optionsObjectType): string {
-
-
-    if (optionsObject.hasRange) {
-        inputString += ` AND (received:>=${getFormattedDate(optionsObject.rangeStart as Date)} AND received:<=${getFormattedDate(optionsObject.rangeEnd as Date)})`;
-    }
-
-    return inputString;
-}
 
 export function makeQuery(
   items: Array<string>,
@@ -49,64 +14,28 @@ export function makeQuery(
     return "empty query";
   }
   if (items.length === 1) {
-    returnedString = `(${items[0]})`;
-    if (optionsObject.hasAttachments) {
-      return returnedString + ` AND hasattachments:yes`;
+    console.log("single item");
+
+    console.log(optionsObject.hasAttachment);
+    if (optionsObject.hasAttachment) {
+      returnedString = `((${items[0]})`;
+      return returnedString + ` AND hasattachments:yes)`;
     } else {
-      return returnedString;
+      return `(${items[0]})`;
     }
   }
-
+  // not a single item so loop it
+  returnedString = `(`;
   for (let i = 0; i < items.length; i++) {
     if (i === items.length - 1) {
       //last
       returnedString += `(${items[i]}))`;
     } else {
-      returnedString += `((${items[i]}) OR `;
+      returnedString += `(${items[i]}) OR `;
     }
   }
-  if (optionsObject.hasAttachments) {
+  if (optionsObject.hasAttachment) {
     returnedString = `(hasattachments:yes AND ` + returnedString;
   }
   return returnedString;
 }
-
-// for date ranged syntax: (received:>=10/1/20 AND received:<=10/5/20)
-
-// for attachment hasattachment:yes
-
-// hasflag:true
-
-// exact: Items containing the exact phrase bob and not the variations such as bobby or bobbin. To search for an exact string, you must use quotation marks.
-// read:no
-
-// export function makeQuery(
-//   items: Array<string>,
-//   optionsObject: optionsObjectType
-// ): string {
-//   let returnedString: string = "";
-//   if (items.length === 0) {
-//     return "empty query";
-//   }
-//   if (items.length === 1) {
-//     returnedString = `(${items[0]})`;
-//     if (optionsObject.hasAttachments) {
-//       return returnedString + ` AND hasattachments:yes`;
-//     } else {
-//       return returnedString;
-//     }
-//   }
-
-//   for (let i = 0; i < items.length; i++) {
-//     if (i === items.length - 1) {
-//       //last
-//       returnedString += `(${items[i]})`;
-//     } else {
-//       returnedString += `(${items[i]}) OR `;
-//     }
-//   }
-//   if (optionsObject.hasAttachments) {
-//     returnedString += ` AND hasattachments:yes`;
-//   }
-//   return returnedString;
-// }
